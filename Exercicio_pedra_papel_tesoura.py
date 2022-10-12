640aa20b540b519ea8ae59d1577d6a2cf5250e60
RodrigodeOliveiraPeixer <rodrigo.oliveira.peixer@gmail.com>
2022-10-12 12:22:33 -0300

    atualização do projeto PPT

--- Exercicio_pedra_papel_tesoura.py
+++ Exercicio_pedra_papel_tesoura.py
@@ -1,3 +1,4 @@
+from ctypes.wintypes import PCHAR
 import tkinter
 from tkinter import *
 from tkinter import ttk
@@ -6,6 +7,8 @@ from tkinter import ttk
 #necessário importar pillow para manipular imagens
 
 from PIL import Image, ImageTk
+
+import random
 
 # cores --------------------------------
 co0 = "#FFFFFF"  # white 
@@ -62,35 +65,188 @@ app_2_linha.place(x=255, y=0)
 
 #linha de empate
 
-app_linha = Label(frame_cima, text="", width=255, anchor='center', font=('ivy 1 bold'), bg=co5, fg=co0)
+app_linha = Label(frame_cima, text="", width=255, anchor='center', font=('ivy 1 bold'), bg=co0, fg=co0)
 app_linha.place(x=0, y=95)
 
 
+#mostrando o que o PC está jogando
 
-#configurando frame baixo botões
+app_PC = Label(frame_baixo, text="", height=1, anchor='center', font=('ivy 10 bold'), bg=co0, fg=co0)
+app_PC.place(x=190, y=10)
 
-icon_1 = Image.open('images/pedra.png')
-icon_1 = icon_1.resize((50,50), Image.ANTIALIAS)
-icon_1 = ImageTk.PhotoImage(icon_1)
-b_icon_1 = Button(frame_baixo, width=50, image=icon_1, compound=CENTER, bg=co0, fg=co0, font=('Ivy 10 bold'), anchor=CENTER, relief= FLAT)
-b_icon_1.place(x=15, y=60)
+#---------------------------------------------------------------------//-------------------------------------------------------------------------
+#variaveis globais
 
-icon_2 = Image.open('images/papel.png')
-icon_2 = icon_2.resize((50,50), Image.ANTIALIAS)
-icon_2 = ImageTk.PhotoImage(icon_2)
-b_icon_2 = Button(frame_baixo, width=50, image=icon_2, compound=CENTER, bg=co0, fg=co0, font=('Ivy 10 bold'), anchor=CENTER, relief= FLAT)
-b_icon_2.place(x=95, y=60)
+global voce 
+global pc
+global jogadas
+global pontos_voce
+global pontos_pc
 
-icon_3 = Image.open('images/tesoura.png')
-icon_3 = icon_3.resize((50,50), Image.ANTIALIAS)
-icon_3 = ImageTk.PhotoImage(icon_3)
-b_icon_3 = Button(frame_baixo, width=50, image=icon_3, compound=CENTER, bg=co0, fg=co0, font=('Ivy 10 bold'), anchor=CENTER, relief= FLAT)
-b_icon_3.place(x=180, y=60)
+pontos_voce = 0
+pontos_pc = 0
+jogadas = 5
 
+
+
+
+#função iniciar o jogo
+
+def  jogar(i):
+    global jogadas
+    global pontos_voce
+    global pontos_pc
+
+    if jogadas > 0:
+        print(jogadas)
+        opcoes = ["Pedra", "Papel", "Tesoura"]
+        pc = random.choice(opcoes)
+
+        voce = i
+
+        app_PC["text"] = pc
+        app_PC["fg"] = co1
+
+        #Em caso de empate
+        if voce == "Pedra" and pc == "Pedra":
+            print("Empate")
+            app_1_linha["bg"] = co0
+            app_2_linha["bg"] = co0
+            app_linha["bg"] = co5
+        
+        elif voce == "Papel" and pc == "Papel":
+            print("Empate")
+            app_1_linha["bg"] = co0
+            app_2_linha["bg"] = co0
+            app_linha["bg"] = co5
+        
+        elif voce == "Tesoura" and pc == "Tesoura":
+            print("Empate")
+            app_1_linha["bg"] = co0
+            app_2_linha["bg"] = co0
+            app_linha["bg"] = co5
+
+
+        #caso não seja empate você escolhendo Pedra !!!!
+
+        elif voce == "Pedra" and pc == "Papel":
+            print("Vitória do PC!!")
+            app_1_linha["bg"] = co7
+            app_2_linha["bg"] = co6
+            app_linha["bg"] = co0
+
+            pontos_pc += 10
+
+        elif voce == "Pedra" and pc == "Tesoura":
+            print("Você ganhou!")
+            app_1_linha["bg"] = co6
+            app_2_linha["bg"] = co7
+            app_linha["bg"] = co0
+
+            pontos_voce += 10
+
+        
+
+        #caso não seja empate você escolhendo Papel !!!!
+
+        elif voce == "Papel" and pc == "Pedra":
+            print("Você ganhou!")
+            app_1_linha["bg"] = co6
+            app_2_linha["bg"] =co7
+            app_linha["bg"] = co0
+
+            pontos_voce += 10
+
+        
+
+        elif voce == "Papel" and pc == "Tesoura":
+            print("Vitória do PC !!")
+            app_1_linha["bg"] = co7
+            app_2_linha["bg"] = co6
+            app_linha["bg"] = co0
+
+            pontos_pc += 10
+
+        #caso não seja empate você escolhendo Tesoura !!!!    
+
+        elif voce == "Tesoura" and pc == "Pedra":
+            print("Vitória do PC !!")
+            app_1_linha["bg"] = co7
+            app_2_linha["bg"] = co6
+            app_linha["bg"] = co0
+
+            pontos_pc += 10
+
+        elif voce == "Tesoura" and pc == "Papel":
+            print("Você ganhou!")
+            app_1_linha["bg"] = co6
+            app_2_linha["bg"] = co7
+            app_linha["bg"] = co0
+
+            pontos_voce += 10
+
+        #atualizando a pontuação
+        app_1_pontos["text"] = pontos_voce
+        app_2_pontos["text"] = pontos_pc
+
+        #atualizando o número de jogadas
+        jogadas -= 1
+
+
+
+
+
+
+    
+    else:
+        fim_do_jogo()
+
+
+
+
+
+
+
+#função lógica do jogo
+
+
+def iniciar_jogo():
+    global icon_1
+    global icon_2
+    global icon_3
+    global b_icon_1
+    global b_icon_2
+    global b_icon_3
+
+    icon_1 = Image.open('images/pedra.png')
+    icon_1 = icon_1.resize((50,50), Image.ANTIALIAS)
+    icon_1 = ImageTk.PhotoImage(icon_1)
+    b_icon_1 = Button(frame_baixo,command=lambda: jogar("Pedra"), width=50, image=icon_1, compound=CENTER, bg=co0, fg=co0, font=('Ivy 10 bold'), anchor=CENTER, relief= FLAT)
+    b_icon_1.place(x=15, y=60)
+
+    icon_2 = Image.open('images/papel.png')
+    icon_2 = icon_2.resize((50,50), Image.ANTIALIAS)
+    icon_2 = ImageTk.PhotoImage(icon_2)
+    b_icon_2 = Button(frame_baixo, command=lambda: jogar("Papel"), width=50, image=icon_2, compound=CENTER, bg=co0, fg=co0, font=('Ivy 10 bold'), anchor=CENTER, relief= FLAT)
+    b_icon_2.place(x=95, y=60)
+
+    icon_3 = Image.open('images/tesoura.png')
+    icon_3 = icon_3.resize((50,50), Image.ANTIALIAS)
+    icon_3 = ImageTk.PhotoImage(icon_3)
+    b_icon_3 = Button(frame_baixo, command=lambda: jogar("Tesoura"), width=50, image=icon_3, compound=CENTER, bg=co0, fg=co0, font=('Ivy 10 bold'), anchor=CENTER, relief= FLAT)
+    b_icon_3.place(x=180, y=60)
+
+#terminar o jogo
+def fim_do_jogo():
+    print("Fim do jogo!")
+
+
+
+#---------------------------------------------------------------------//-------------------------------------------------------------------------
 
 #configurando botão iniciar
 
-b_jogar = Button(frame_baixo, width=30, text='Jogar', bg=fundo, fg=co0, font=('Ivy 10 bold'), anchor=CENTER, relief= RAISED, overrelief=RIDGE)
+b_jogar = Button(frame_baixo, command= iniciar_jogo, width=30, text='Jogar', bg=fundo, fg=co0, font=('Ivy 10 bold'), anchor=CENTER, relief= RAISED, overrelief=RIDGE)
 b_jogar.place(x=5, y=151)
 
 
